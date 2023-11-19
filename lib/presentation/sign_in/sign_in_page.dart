e418987d5b016a624a46b6d29f1799450a66fe38
Temirlan <brucewaynetemir@gmail.com>
2023-11-19 13:38:26 +0600

    refactor: sign_in_page

--- lib/presentation/sign_in/sign_in_page.dart
+++ lib/presentation/sign_in/sign_in_page.dart
@@ -1,71 +1,105 @@
 import 'package:chat/application/auth/sign_in/sign_in_cubit.dart';
-import 'package:chat/presentation/chat/chat_overview/chat_overivew_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../chat/chat_overview/chat_overivew_page.dart';
 
 class SignInPage extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     final signInCubit = context.read<SignInCubit>();
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Sign In'),
+        backgroundColor: Color(0xff1b252f),
+        title: Center(child: const Text('Log In')),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: BlocConsumer<SignInCubit, SignInState>(
-          listener: (context, state) {
-            state.when(
-              success: () {
-               Navigator.push(context, MaterialPageRoute(builder: (context) => ChatOverviewPage(),));
-              },
-              error: (errorMessage) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text(errorMessage)),
-                );
-              },
-             initial: () {  }, loading: () {  },
-            );
-          },
-          builder: (context, state) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'Email',
-                    border: OutlineInputBorder(),
+        child: Form(
+          key: _formKey,
+          child: BlocConsumer<SignInCubit, SignInState>(
+            listener: (context, state) {
+              state.when(
+                success: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ChatOverviewPage(),
+                    ),
+                  );
+                },
+                error: (errorMessage) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text(errorMessage)),
+                  );
+                },
+                initial: () {},
+                loading: () {},
+              );
+            },
+            builder: (context, state) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Email',
+                      border: OutlineInputBorder(),
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    onChanged: (value) {
+                      // Logic to update email
+                    },
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your email';
+                      } else if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(value)) {
+                        return 'Please enter a valid email';
+                      }
+                      return null;
+                    },
                   ),
-                  keyboardType: TextInputType.emailAddress,
-                  onChanged: (value) {
-                    // Logic to update email
-                  },
-                ),
-                const SizedBox(height: 16.0),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'Password',
-                    border: OutlineInputBorder(),
+                  const SizedBox(height: 16.0),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                      border: OutlineInputBorder(),
+                    ),
+                    obscureText: true,
+                    onChanged: (value) {
+                      // Logic to update password
+                    },
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your password';
+                      } else if (value.length < 8) {
+                        return 'Password must be at least 8 characters long';
+                      }
+                      return null;
+                    },
                   ),
-                  obscureText: true,
-                  onChanged: (value) {
-                    // Logic to update password
-                  },
-                ),
-                const SizedBox(height: 24.0),
-                ElevatedButton(
-                  onPressed: () {
-                    signInCubit.signIn('', '');
-                  },
-                  child: const Text('Sign In'),
-                ),
-                // Other UI elements
-              ],
-            );
-          },
+                  const SizedBox(height: 32.0),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        signInCubit.signIn('', ''); // Ваша логика входа
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xff1b252f),
+                      minimumSize: Size(double.infinity, 50),
+                    ),
+                    child: const Text('Sign In'),
+                  ),
+                  // Other UI elements
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
