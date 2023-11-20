f7406ebc1b88899e470e7bf0015497cd1ef741ba
Temirlan <brucewaynetemir@gmail.com>
2023-11-20 23:20:04 +0600

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,5 @@ import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(DevicePreview(builder: (context) =>  AppWidget()));
+  runApp(AppWidget());
 }
